2acdf1ecf790d89ef73bae70db955149a586e043
Damian Jacewicz <damian.jacewicz@tomtom.com>
2019-11-26 13:52:58 +0100

    hermetyzacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,31 +4,38 @@ public class DefaultCountingOutRhymer {
 
     private final int[] NUMBERS = new int[12];
 
-    public int total = -1;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            getNUMBERS()[++total] = in;
     }
 
-    public boolean callCheck() {
+    boolean callCheck() {
         return total == -1;
     }
 
-    public boolean isFull() {
+    boolean isFull() {
         return total == 11;
     }
 
-    protected int peekaboo() {
+    int peekaboo() {
         if (callCheck())
             return -1;
-        return NUMBERS[total];
+        return getNUMBERS()[total];
     }
 
     public int countOut() {
         if (callCheck())
             return -1;
-        return NUMBERS[total--];
+        return getNUMBERS()[total--];
     }
 
+    public int getTotal() {
+        return total;
+    }
+
+    public int[] getNUMBERS() {
+        return NUMBERS;
+    }
 }

cff30cd7bce7ad1d2caa8974e014a772906850e7
Damian Jacewicz <damian.jacewicz@tomtom.com>
2020-01-14 14:10:16 +0100

    a little cleanup

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -2,21 +2,18 @@ package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
 
-    public DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
+    private DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
     @Override
     public int countOut() {
         while (!callCheck())
-
             temp.countIn(super.countOut());
 
         int ret = temp.countOut();
 
         while (!temp.callCheck())
-
             countIn(temp.countOut());
 
         return ret;
     }
 }
-//alt strzlka zmienia okno w javie
